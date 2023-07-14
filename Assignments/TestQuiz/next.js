8335694412ecb00d3d781cc89738cf0095d1edb8
Ponchanon Datta Rone <51744698+ponchanon@users.noreply.github.com>
2023-07-13 21:12:45 -0500

    updated
    updatedu

--- Assignments/TestQuiz/next.js
+++ Assignments/TestQuiz/next.js
@@ -1,6 +1,14 @@
-setImmediate(()=>console.log("Imm"));
-setTimeout(function() {}, 2000);
-setTimeout(()=>console.log("time"));
-setTimeout(function() {}, 2000);
-process.nextTick(()=>console.log("next"));
-setTimeout(function() {}, 2000);
+// setImmediate(()=>console.log("Imm"));
+// setTimeout(function() {}, 2000);
+// setTimeout(()=>console.log("time"));
+// setTimeout(function() {}, 2000);
+// process.nextTick(()=>console.log("next"));
+// setTimeout(function() {}, 2000);
+
+
+function sum(...numbers) {
+    //return numbers.reduce((total, num) => total + num, 0);
+    return numbers;
+}
+
+console.log(sum(1, 2, 3, 4, 5)); // Output: 15
